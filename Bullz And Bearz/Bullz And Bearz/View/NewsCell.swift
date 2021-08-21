4f0c9bcd4c56f8dc195eca71ebbdd88bad94340d
Utkarsh Sharma <skhiearth@gmail.com>
2021-08-21 21:13:11 +0530

    custom cells
    Co-Authored-By: Simran Gogia <70960753+simmsss@users.noreply.github.com>

---
+++ Bullz And Bearz/Bullz And Bearz/View/NewsCell.swift
@@ -0,0 +1,11 @@
+//
+//  NewsCell.swift
+//  Bullz And Bearz
+//
+
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+    @IBOutlet weak var newsTitleLbl: UILabel!
+}
---
+++ Bullz And Bearz/Bullz And Bearz/View/SymbolCell.swift
@@ -0,0 +1,22 @@
+//
+//  SymbolCell.swift
+//  Bullz And Bearz
+//
+
+import UIKit
+
+class SymbolCell: UITableViewCell {
+
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
